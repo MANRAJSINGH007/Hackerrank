556e96c8c0332fa7a9cf385782406d13780af235
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-06-05 23:53:53 +0530

    Create ice_cream_parlor.java

---
+++ ice_cream_parlor.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the icecreamParlor function below.
+    static int[] icecreamParlor(int m, int[] a) {
+        
+        int n=a.length;
+        int b[]=new int[2];
+        for(int i=0;i<n-1;i++)
+        {
+            if(a[i]>m)
+            {
+                i++;
+                if(i==n-1)
+                {
+                    break;
+                }
+            }
+            
+            int key=m-a[i];
+            for(int j=i+1;j<n;j++)
+            {
+                if(a[j]==key)
+                {
+                    b[0]=i+1;
+                    b[1]=j+1;
+                }
+            }
+        }
+        
+        
+        return b;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int m = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] arr = new int[n];
+
+            String[] arrItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr[i] = arrItem;
+            }
+
+            int[] result = icecreamParlor(m, arr);
+
+            for (int i = 0; i < result.length; i++) {
+                bufferedWriter.write(String.valueOf(result[i]));
+
+                if (i != result.length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
